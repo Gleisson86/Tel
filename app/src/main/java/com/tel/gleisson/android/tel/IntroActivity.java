59a75383dbac13f48b8384a8d2dd9416f4786779
Gleisson86 <gleissonqi200@gmail.com>
2016-10-07 23:15:17 -0300

    Intro v1.1

---
+++ app/src/main/java/com/tel/gleisson/android/tel/IntroActivity.java
@@ -0,0 +1,51 @@
+package com.tel.gleisson.android.tel;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+
+
+import com.github.paolorotolo.appintro.AppIntro;
+
+/**
+ * Created by Gleisson e Rosy on 07/10/2016.
+ */
+
+public class IntroActivity extends AppIntro {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addSlide(SampleSlide.newInstance(R.layout.intro1));
+        addSlide(SampleSlide.newInstance(R.layout.intro2));
+        addSlide(SampleSlide.newInstance(R.layout.intro3));
+
+        setFlowAnimation();
+
+    }
+
+
+        @Override
+
+        public void onSkipPressed(Fragment currentFragment ){
+            super.onSkipPressed(currentFragment);
+            finish();
+        }
+
+        @Override
+        public void onDonePressed(Fragment currentFragment){
+            super.onDonePressed(currentFragment);
+           finish();
+        }
+
+        @Override
+        public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment){
+            super.onSlideChanged(oldFragment, newFragment);
+        }
+
+    }
+
--- app/src/main/java/com/tel/gleisson/android/tel/MainActivity.java
+++ app/src/main/java/com/tel/gleisson/android/tel/MainActivity.java
@@ -1,5 +1,6 @@
 package com.tel.gleisson.android.tel;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, IntroActivity.class);
+        this.startActivity(intent);
+
     }
 }
