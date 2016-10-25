b4615f6d28f967bb553f7f3a810bc9ce5b55b90b
Gleisson86 <gleissonqi200@gmail.com>
2016-10-25 00:41:53 -0200

    Tela inicial 1.0

--- app/src/main/java/com/tel/gleisson/android/tel/CriarContaActivity.java
+++ app/src/main/java/com/tel/gleisson/android/tel/CriarContaActivity.java
@@ -16,6 +16,10 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 /**
  * Created by Gleisson e Rosy on 14/10/2016.
@@ -29,11 +33,13 @@ public class CriarContaActivity extends AppCompatActivity implements View.OnClic
     private EditText editTextPassword;
     private Button botaoCriarConta;
     private ProgressDialog progressDialog;
+    private ClasseUtil classeUtil;
 
 
     //defining firebaseauth object
+    private DatabaseReference mDatabase;
     private FirebaseAuth firebaseAuth;
-
+    private FirebaseUser user;
 
 
     // Método onCreate
@@ -45,10 +51,11 @@ public class CriarContaActivity extends AppCompatActivity implements View.OnClic
 
 
         firebaseAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        editTextNome = (EditText) findViewById (R.id.name);
-        editTextEmail = (EditText) findViewById (R.id.email);
-        editTextPassword = (EditText) findViewById (R.id.senha);
+        editTextNome = (EditText) findViewById(R.id.name);
+        editTextEmail = (EditText) findViewById(R.id.email);
+        editTextPassword = (EditText) findViewById(R.id.senha);
         botaoCriarConta = (Button) findViewById(R.id.botao_criar_conta);
 
         progressDialog = new ProgressDialog(this);
@@ -58,73 +65,89 @@ public class CriarContaActivity extends AppCompatActivity implements View.OnClic
     }
 
 
-    //--Chama Introdução
-    //-------------------------------------------------------------------------
-    private void chamaIntro(){
-        Intent intent = new Intent(this, IntroActivity.class);
-        this.startActivity(intent);
-    }
-
-
-
     // Metodo para criar uma conta baseada em Email e Senha
     //------------------------------------------------------
 
-    private void registerUser(){
+    private void registerUser() {
+
+
 
         //getting email and password from edit texts
+        String nome = editTextNome.getText().toString().trim();
         String email = editTextEmail.getText().toString().trim();
-        String password  = editTextPassword.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        classeUtil = new ClasseUtil(this);
 
-        //checking if email and passwords are empty
-        if(TextUtils.isEmpty(email)){
-            Toast.makeText(this,"Please enter email",Toast.LENGTH_LONG).show();
+        if (TextUtils.isEmpty(nome)) {
+            classeUtil.chamaDialogo(getString(R.string.nomeCompletoVazio), getString(R.string.tentenovamente));
             return;
         }
 
-        if(TextUtils.isEmpty(password)){
-            Toast.makeText(this,"Please enter password",Toast.LENGTH_LONG).show();
+        if (TextUtils.isEmpty(email)) {
+            classeUtil.chamaDialogo(getString(R.string.entreComEmailMensagem), getString(R.string.tentenovamente));
             return;
         }
+        if (TextUtils.isEmpty(password)) {
+            classeUtil.chamaDialogo(getString(R.string.entreComSenhaTitulo), getString(R.string.tentenovamente));
+            return;
+        }
+
 
         //if the email and password are not empty
         //displaying a progress dialog
-
-        progressDialog.setMessage(getString(R.string.aguarde_registro_conta));
-        progressDialog.setTitle("Registrando..");
-        progressDialog.show();
+        classeUtil.chamaProgess(getString(R.string.processandoTitulo), getString(R.string.aguardeMensagem));
 
         //creating a new user
         firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 //checking if success
+                classeUtil.chamaProgresFim();
 
-                if(task.isSuccessful()){
+                if (task.isSuccessful()) {
                     //display some message here
-                    Toast.makeText(CriarContaActivity.this,getString(R.string.registro_sucesso),Toast.LENGTH_LONG).show();
-                    startActivity( new Intent(CriarContaActivity.this, MainActivity.class));
-                   finish();
-                }else{
+                    criaProfile(task.getResult().getUser());
+                    Toast.makeText(CriarContaActivity.this,getString(R.string.conta_criada_sucesso),Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(CriarContaActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
                     //display some message here
-                    Toast.makeText(CriarContaActivity.this,"Registration Error",Toast.LENGTH_LONG).show();
 
+                    classeUtil.chamaDialogo(getString(R.string.falhaAutenticacaoTitulo), getString(R.string.falhaAutenticacaoMensagem));
+                    //    Toast.makeText(CriarContaActivity.this,"Registration Error",Toast.LENGTH_LONG).show();
                 }
-                progressDialog.dismiss();
+
             }
         });
 
     }
 
+    //CRIA PROFILE DE USUÁRIO
+    //-------------------------------------------------------------------------------------------------------
+
+    private void criaProfile(FirebaseUser user) {
 
 
-    // Método que chama o Criar Nova Conta assim que o usuário
-    //clica no botão Criar
-    //----------------------------------------------------------------
+        String username = editTextNome.getText().toString().trim();
+        writeNewUser(user.getUid(), username, user.getEmail());
+    }
+
+    // [START basic_write]
+    private void writeNewUser(String userId, String name, String email) {
+        classeUtil = new ClasseUtil(this);
+        User user = new User(name, email);
+        mDatabase.child("users").child(userId).setValue(user);
+
+
+    }
+    // [END basic_write]
+
+
 
     @Override
     public void onClick(View view) {
         //calling register method on click
-     registerUser();
+        registerUser();
     }
 }
--- app/src/main/java/com/tel/gleisson/android/tel/MainActivity.java
+++ app/src/main/java/com/tel/gleisson/android/tel/MainActivity.java
@@ -2,14 +2,31 @@ package com.tel.gleisson.android.tel;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity  {
 
     private PrefManager prefManager;
     private FirebaseAuth auth;
+    private ClasseUtil classeUtil;
+    private FirebaseUser user;
+
+    private Button sair;
+    private Button deletarConta;
 
     //  SharedPreference regra para viu ou não viu a intro. Dentro da on create e também
     // no onResume.
@@ -19,18 +36,120 @@ public class MainActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        auth = FirebaseAuth.getInstance();
         prefManager = new PrefManager(this);
+        classeUtil = new ClasseUtil(this);
         setContentView(R.layout.telainicial_main);
 
+      //  sair = (Button) findViewById(R.id.sair);
+     //   deletarConta = (Button) findViewById(R.id.deletarConta);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+
+        // Adding Toolbar to Main screen
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Setting ViewPager for each Tabs
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+        // Set Tabs inside Toolbar
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+
+
         // Checking for first time launch - before calling setContentView()
-
-
         if (prefManager.isFirstTimeLaunch()) {
             launchIntroScreen();
         }
 
-        auth = FirebaseAuth.getInstance();
+        /*
+        sair.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+                public void onClick(View v) {
+
+           //     classeUtil.chamaDialogo(null,getString(R.string.voceRealmenteSair));
+                    auth.signOut();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+        deletarConta.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                   //  classeUtil.chamaDialogo(null,getString(R.string.deletarContaMensagem));
+                     classeUtil.chamaProgess(getString(R.string.aguardeTitulo),getString(R.string.processandoTitulo));
+                user.delete()
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                 classeUtil.chamaProgresFim();
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(MainActivity.this,getString(R.string.contaExcluidaMensagem),Toast.LENGTH_LONG).show();
+                                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                                else {
+                                    Toast.makeText(MainActivity.this, getString(R.string.falhaAoDeletarConta), Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+
+
+            }
+        });      */
+
+
+
+
+
     }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public Adapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+            return  mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+
+
+    private void setupViewPager(ViewPager viewPager) {
+        Adapter adapter = new Adapter(getSupportFragmentManager());
+      //  adapter.addFragment(new ListContentFragment(), "List");
+      //  adapter.addFragment(new TileContentFragment(), "Tile");
+        adapter.addFragment(new CardFraguimentoEquipamento(), "Equipamento");
+        viewPager.setAdapter(adapter);
+    }
+
 
     private void launchIntroScreen() {
         startActivity(new Intent(MainActivity.this, IntroActivity.class));
@@ -49,8 +168,5 @@ public class MainActivity extends AppCompatActivity  {
     }
     //---- Fim SharedPreference
     //----------------------------------------------------
-
-
-
 
 }
